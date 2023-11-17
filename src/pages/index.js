11822650be9db894e68179462cb4335cab5c0d61
midhunvnadh <midhun.vnadh5732@gmail.com>
2023-11-17 19:09:33 +0530

    Done for new UI

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,16 +1,30 @@
 import axios from "axios";
-import React, { useState } from "react";
+import Image from "next/image";
+import React, { useState, useEffect, useRef } from "react";
+
+const useFocus = () => {
+  const htmlElRef = useRef(null);
+  const setFocus = () => {
+    htmlElRef.current && htmlElRef.current.focus();
+  };
+
+  return [htmlElRef, setFocus];
+};
 
 export default function Home() {
   const [results, setResults] = useState([]);
   const [loading, setLoading] = useState(false);
   const [searchTerm, setSearchTerm] = useState("");
+  const [inputRef, setInputFocus] = useFocus();
+  const [previousSearchTerm, setPreviousSearchTerm] = useState("");
 
   const search = (e) => {
     e.preventDefault();
     setLoading(true);
+    const searchT = searchTerm || previousSearchTerm;
+    if (!searchT) return;
     axios
-      .get(`/api/product/${searchTerm}`)
+      .get(`/api/product/${searchT}`)
       .then(({ data }) => {
         setResults(data);
         setLoading(false);
@@ -20,9 +34,15 @@ export default function Home() {
         alert("Error fetching data");
       })
       .finally(() => {
+        setPreviousSearchTerm(searchT);
+        setSearchTerm("");
         setLoading(false);
       });
   };
+
+  useEffect(() => {
+    if (searchTerm === "") setInputFocus();
+  }, [searchTerm, setInputFocus]);
 
   return (
     <div>
@@ -36,12 +56,17 @@ export default function Home() {
         >
           <div className="w-3/4">
             <input
+              ref={inputRef}
               type="number"
               className="w-full rounded-md p-3 outline-none outline-none transition-all hover:ring-2 hover:ring-blue-600 hover:ring-opacity-50"
-              placeholder="Enter Barcode number / Scan with a reader"
+              placeholder={
+                previousSearchTerm ||
+                "Enter Barcode number / Scan with a reader"
+              }
               onChange={({ target }) => {
                 setSearchTerm(target.value);
               }}
+              value={searchTerm}
             />
           </div>
           <div>
@@ -64,12 +89,34 @@ export default function Home() {
                 {!results.main &&
                   results.others &&
                   results.others.length === 0 && <div>No results found</div>}
-                {results.main && (
+                {results.main && results.main.length > 0 && (
                   <div className="bg-gray-300 p-3 rounded-md">
                     <div className="mb-3">
                       <h2 className="font-bold text-3xl">Main Result</h2>
                     </div>
-                    <div>{<div>{results.main}</div>}</div>
+                    <div className="flex items-center justify-start flex-wrap w-full">
+                      {results.main.map(({ engine, name, image }, i) => (
+                        <div
+                          className="lg:w-1/4 w-full p-3"
+                          key={`result-m-${i}-${name}`}
+                        >
+                          <div className="flex items-center flex-col gap-2 bg-white  p-3 rounded-md w-full h-full">
+                            <div className="w-full">
+                              <img
+                                src={image || "/placeholder.webp"}
+                                alt={name}
+                              />
+                            </div>
+                            <div className="border-t w-full"></div>
+                            <div className="">
+                              <div className="font-bold">{engine}</div>
+                            </div>
+                            <div className="border-t w-full"></div>
+                            <div className="w-full">{name}</div>
+                          </div>
+                        </div>
+                      ))}
+                    </div>
                   </div>
                 )}
                 {results.others && results.others.length > 0 && (
@@ -77,9 +124,27 @@ export default function Home() {
                     <div className="mb-3">
                       <h2 className="font-bold text-3xl">Other Results</h2>
                     </div>
-                    <div>
-                      {results.others.map((result, i) => (
-                        <div key={`result-${i}-${result}`}>{result}</div>
+                    <div className="flex items-center justify-start flex-wrap w-full">
+                      {results.others.map(({ engine, name, image }, i) => (
+                        <div
+                          className="lg:w-1/4 w-full p-3"
+                          key={`result-o-${i}-${name}`}
+                        >
+                          <div className="flex items-center flex-col gap-2 bg-white  p-3 rounded-md w-full h-full">
+                            <div className="w-full">
+                              <img
+                                src={image || "/placeholder.webp"}
+                                alt={name}
+                              />
+                            </div>
+                            <div className="border-t w-full"></div>
+                            <div className="">
+                              <div className="font-bold">{engine}</div>
+                            </div>
+                            <div className="border-t w-full"></div>
+                            <div className="w-full">{name}</div>
+                          </div>
+                        </div>
                       ))}
                     </div>
                   </div>
